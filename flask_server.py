722c46daf41b1125a689ba1bd3b238071d195c12
Julien Chiapp <julien.chiappinelli@epfl.ch>
2018-03-10 14:00:44 +0100

    cookie test

--- flask_server.py
+++ flask_server.py
@@ -1,18 +1,32 @@
 #!user/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, make_response
 
 app = Flask(__name__)
 
-#def restart():
-#    global app
-#    app = Flask(__name__, template_folder="templates" )
+
+# Global variables
+
+ip_list = list()
+ip = str()
+
+def restart():
+    global app
+    app = Flask(__name__, template_folder="templates" )
 
 
 @app.route('/')
 def index():
     title = "Bienvenu sur ryben network"
-    return render_template('index.html', index_title = title)
+    if request.remote_addr not in ip_list:
+        ip_list.append(request.remote_addr)
+    ip = str(request.remote_addr)
+
+    # sets cookies to remember user
+    resp = make_response(render_template('index.html', index_title = title))
+    resp.set_cookie( 'user' , ip )
+    return resp
+    #return render_template('index.html', index_title = title)
 
 @app.route('/stats')
 def stats():
@@ -28,6 +42,11 @@ def show():
     dict = {'phy': 50, 'che': 60, 'maths': 70}
     print(dict)
     return render_template('show.html', result=dict)
+
+@app.route('/cookie/')
+def get_cookie():
+    return jsonify(username = request.cookies.get('user')), 200
+
 
 
 if __name__ == '__main__':
